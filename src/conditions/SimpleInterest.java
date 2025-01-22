7b474a5ab23bc1c0071973d370d358e941193f7f
prathi-pt7872 <prathibhapv2@gmail.com>
2025-01-22 14:51:47 +0530

    first commit

--- src/conditions/SimpleInterest.java
+++ src/conditions/SimpleInterest.java
@@ -2,16 +2,20 @@ package conditions;
 import java.util.Scanner;
 
 public class SimpleInterest {
-    public static void main(String [] args){
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("To calculate simple interest");
-        System.out.println("Enter principle value : ");
-        int num = scanner.nextInt();
+        System.out.println("Enter principle : ");
+        int p = scanner.nextInt();
 
-        System.out.println("Enter string to check whether it is palindrome : ");
-        int num = scanner.nextInt();
+        System.out.println("Enter interest : ");
+        int n = scanner.nextInt();
 
-        System.out.println("Enter string to check whether it is palindrome : ");
-        int num = scanner.nextInt();
+        System.out.println("Enter rate: ");
+        int r = scanner.nextInt();
+
+        int simple_interest = (p * n * r) / 100;
+        System.out.println("Simple interest value is : " + simple_interest);
+    }
 }
