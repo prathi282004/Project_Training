ad699d1712361a0d82f1162845016b46523ba946
prathi-pt7872 <prathibhapv2@gmail.com>
2025-01-23 10:13:44 +0530

    first commit o

--- src/arrays/SumOfElements.java
+++ src/arrays/SumOfElements.java
@@ -2,10 +2,9 @@ package arrays;
 
 public class SumOfElements {
     public static void main(String [] args) {
-        int arr[] = {2, 56, 4, 9, 0, 64, -8};
+        int[] arr = {2, 56, 4, 9, 0, 64, -8};
 
         int sum = 0;
-        int a = 0;
 
         for (int i = 0; i < arr.length; i++) {
             sum = sum + arr[i];
