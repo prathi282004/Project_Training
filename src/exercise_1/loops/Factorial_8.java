d284161aeaed0911ab6b76d9f4ea5a026e2ec752
prathi-pt7872 <prathibhapv2@gmail.com>
2025-01-29 19:22:11 +0530

    first commit o

--- src/exercise_1/loops/Factorial_8.java
+++ src/exercise_1/loops/Factorial_8.java
@@ -1,7 +1,7 @@
 // 8. *Factorial*
 //   Write a program to find the factorial of a given number using a loop.
 
-package exercise_1.Loops;
+package exercise_1.loops;
 import java.util.Scanner;
 // JR: review done
 //      1) output coule be " 1 x 2 x 3 x 4 = 24 " (if the given number is small like below 10)
@@ -12,10 +12,17 @@ public class Factorial_8 {
         System.out.println("Enter value to calculate factorial : ");
         int n = scanner.nextInt();
 
-        int fact =1;
+       int fact =1;
+
         for(int i=1; i<=n; i++){
             fact = fact * i;
+            if (n < 11){
+                System.out.print(i);
+                if (i < n) {
+                    System.out.print(" * ");
+                }
+            }
         }
-        System.out.println("Factorial of " + n + " is : " + fact);
+        System.out.println("\nFactorial of " + n + " is : " + fact);
     }
 }
