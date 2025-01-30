25f37b9fe78748bb25714f90fb173bfc2d0d285c
prathi-pt7872 <prathibhapv2@gmail.com>
2025-01-30 18:23:49 +0530

    first commit o

--- src/exercise_1/conditions/AmstrongNumber_14_1.java
+++ src/exercise_1/conditions/AmstrongNumber_14_1.java
@@ -3,12 +3,15 @@
 //    Example: 153 = 1³ + 5³ + 3³
 
 package exercise_1.conditions;
+
 import java.util.Scanner;
+
 // JR: TODO
 //      1)not working if we give digits as 23 and number as 153 - it works uncle!!!
-//      JR - digit -to- number validation is not done.
+//      JR - digit -to- number validation is not done. - doneeeeeee
+
 public class AmstrongNumber_14_1 {
-    public static void main(String [] args){
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Enter number of digits to check amstrong value : ");
@@ -17,23 +20,25 @@ public class AmstrongNumber_14_1 {
         System.out.print("Enter a number: ");
         int num = scanner.nextInt();
 
+        int length = String.valueOf(num).length();
+
         int sum = 0;
         int temp = num;
 
-        while (temp > 0) {
-            int a = temp % 10;
-            sum += Math.pow(a, n);
-            temp = temp / 10;
+        if (n == length) {
+            while (temp > 0) {
+                int a = temp % 10;
+                sum += (int) Math.pow(a, n);
+                temp = temp / 10;
+
+                if (sum == num) {
+                    System.out.println(num + " is a Amstrong number");
+                } else {
+                    System.out.println(num + " is Not a Amstrong number");
+                }
+            }
+        } else {
+            System.out.println("Invalid input!!");
         }
-
-
-        if(sum == num) {
-            System.out.println(num + " is a Amstrong number");
-        }
-        else{
-            System.out.println(num + " is Not a Amstrong number");
-        }
-
-
     }
 }
