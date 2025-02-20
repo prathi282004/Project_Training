ba846a5f50874b52f3cda7fd26327c8747ddba46
prathi282004 <prathibhapv2@gmail.com>
2025-02-20 10:27:20 +0530

    Commit

---
+++ src/excercise_4/HollowSquare.java
@@ -0,0 +1,24 @@
+package excercise_4;
+
+import java.util.Scanner;
+
+public class HollowSquare {
+    public static void main(String[] args) {
+        System.out.println("Hollow Square with diagonal Pattern");
+        System.out.println("Enter n value to print pattern : ");
+
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || i == n - 1 || j == 0 || j == n - 1 || i == j || i + j == n - 1) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
